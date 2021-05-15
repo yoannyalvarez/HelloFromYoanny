3ad3fb16f73753d2f4252953aa0f735fffb2d767
imasmilergirl <imasmilergirl@hotmail.com>
2021-05-15 16:43:42 -0500

    second commitment

--- src/alvarez/Main.java
+++ src/alvarez/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello, my name is Yoanny");
-
         System.out.println("Hello Yoanny my name is Manuel.");
+        System.out.println("This my second commitment.");
     }
 }
